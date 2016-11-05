17628c909fc19eb3bdfa91835d0b376d224d3349
e-foley <efoley@calpoly.edu>
2016-11-04 23:48:40 -0700

    place pieces at cursor

--- BoardDrawerTestApplet.java
+++ BoardDrawerTestApplet.java
@@ -11,62 +11,131 @@ import java.awt.Dimension;
 import java.util.Random;
 
 /**
-   This applet lets the user move a rectangle by clicking
-   the mouse.
-*/
+This applet lets the user move a rectangle by clicking
+the mouse.
+ */
 public class BoardDrawerTestApplet extends Applet
 {  
-   public BoardDrawerTestApplet()
-   {  
-       camera = new CameraPosition();
-       board = new GameBoard();
-       bank = new StandardTileBank();
-       randomizer = new Random();
-       board.addPiece(0, 0, new GamePiece(1, 0));
-       //board.addPiece(1, 0, new GamePiece(0, 1));
-       //board.addPiece(2, 0, new GamePiece(1, 0));
-       //board.addPiece(2, 1, new GamePiece(1, 1));
+    public BoardDrawerTestApplet()
+    {  
+        camera = new CameraPosition();
+        board = new GameBoard();
+        bank = new StandardTileBank();
+        randomizer = new Random();
+        counter_x = 0;
+        counter_y = 0;
+        cursor_normal_x = 0.0;
+        cursor_normal_y = 0.0;
+        cursor_board_coords_x = 0;
+        cursor_board_coords_y = 0;
+        
+        // Just so we don't start at weird zoom!
+        board.addPiece(cursor_board_coords_x, cursor_board_coords_y, bank.drawRandom(randomizer));
+        repaint();
 
-       counter_x = 0;
-       counter_y = 0;
-      // the rectangle that the paint method draws
-      icon = new SquareIcon(Color.RED);
-         
-      // add mouse press listener         
+        class MousePressListener implements MouseListener
+        {  
+            public void mousePressed(MouseEvent event)
+            {  
+                int x = event.getX();
+                int y = event.getY();
+                // box.setLocation(x, y);
 
-      class MousePressListener implements MouseListener
-      {  
-         public void mousePressed(MouseEvent event)
-         {  
-            int x = event.getX();
-            int y = event.getY();
-            // box.setLocation(x, y);
-            
-            if (event.getButton() == MouseEvent.BUTTON1 && !bank.isEmpty()) {
-                board.addPiece(++counter_x, counter_y, bank.drawRandom(randomizer));
-                repaint();
-            } else if (event.getButton() == MouseEvent.BUTTON3 && !bank.isEmpty()) {
-                board.addPiece(counter_x, ++counter_y, bank.drawRandom(randomizer));
-                repaint();
+                recalculateNormalCoordinates(x, y);
+                
+                if (event.getButton() == MouseEvent.BUTTON1 && !bank.isEmpty() && !board.isPieceAt(cursor_board_coords_x, cursor_board_coords_y)) {
+                    board.addPiece(cursor_board_coords_x, cursor_board_coords_y, bank.drawRandom(randomizer));
+                    repaint();
+                } else if (event.getButton() == MouseEvent.BUTTON3 && board.isPieceAt(cursor_board_coords_x, cursor_board_coords_y)) {
+                    // GamePiece piece_removing = board.getPiece(cursor_board_coords_x, cursor_board_coords_y);
+                    GamePiece piece_removing = board.removePiece(cursor_board_coords_x, cursor_board_coords_y);
+                    bank.addPiece(piece_removing);
+                    repaint();
+                }
+//                 if (event.getButton() == MouseEvent.BUTTON1 && !bank.isEmpty()) {
+//                     board.addPiece(++counter_x, counter_y, bank.drawRandom(randomizer));
+//                     repaint();
+//                 } else if (event.getButton() == MouseEvent.BUTTON3 && !bank.isEmpty()) {
+//                     board.addPiece(counter_x, ++counter_y, bank.drawRandom(randomizer));
+//                     repaint();
+//                 }
+                //board.addPiece(x, y, new GamePiece(0, 0));
+
             }
-            //board.addPiece(x, y, new GamePiece(0, 0));
-            
 
-         }
+            // do-nothing methods
+            public void mouseReleased(MouseEvent event) {}
 
-         // do-nothing methods
-         public void mouseReleased(MouseEvent event) {}
-         public void mouseClicked(MouseEvent event) {}
-         public void mouseEntered(MouseEvent event) {}
-         public void mouseExited(MouseEvent event) {}
-      }
-      
+            public void mouseClicked(MouseEvent event) {}
 
-         
-      MouseListener listener = new MousePressListener();
-      addMouseListener(listener);
-   }
+            public void mouseEntered(MouseEvent event) {}
 
+            public void mouseExited(MouseEvent event) {}
+        }
+
+        class MouseMoveListener implements MouseMotionListener
+        {  
+            public void mouseMoved(MouseEvent event)
+            {
+                int x = event.getX();
+                int y = event.getY();
+                if(recalculateNormalCoordinates(x, y)) {
+                    repaint();
+                }
+            }
+
+            public void mouseDragged(MouseEvent event) {}
+            //          public void mouseDragged(MouseEvent event)
+            //          {
+            //              event = snapToGrid(event);
+            //              if (firstButtonHeld)
+            //              {     
+            //                 makeSpecialLines(event.getX(), event.getY());
+            //                 repaint();
+            //              }
+            //          }
+        }
+
+        MouseListener listener = new MousePressListener();
+        MouseMotionListener move_listener = new MouseMoveListener();
+        addMouseListener(listener);
+        addMouseMotionListener(move_listener);
+    }
+
+    // Returns true if the value is different than before
+    public boolean recalculateNormalCoordinates(int x, int y)
+    {
+        // TODO: Consider whether the camera should hold this information.
+        Dimension applet_size = this.getSize();
+        int applet_height = applet_size.height;
+        int applet_width = applet_size.width;
+        
+        // Yeah, it really seems like the camera should be able to handle the width/height calculations.
+        cursor_normal_x = camera.inverseXPosition(x - applet_width / 2.0);
+        cursor_normal_y = camera.inverseYPosition(y - applet_height / 2.0);
+        DenormalizationResult result = BoardDrawer.denormalize(cursor_normal_x, cursor_normal_y, true);
+        boolean new_value = false;
+        // TODO: Consider whether we actually want to mirror these fields in the applet class or whether we should just store the denormalization result
+        if (result.success) {
+            valid_board_coords = true;
+            if (cursor_board_coords_x != result.value_x)
+            {
+                cursor_board_coords_x = result.value_x;
+                new_value = true;
+            }
+            if (cursor_board_coords_y != result.value_y)
+            {
+                cursor_board_coords_y = result.value_y;
+                new_value = true;
+            }
+        } else {
+            valid_board_coords = false;
+        }
+        
+        return new_value;
+    }
+
+    // TODO: Remove zooming from paint function.  This should occur elsewhere.
     public void paint(Graphics g)
     {  
         Dimension applet_size = this.getSize();
@@ -83,18 +152,30 @@ public class BoardDrawerTestApplet extends Applet
         camera.setYCenter(BoardDrawer.getNormalBoardCenterY(board));
         //camera.setXCenter(0.0);
         //camera.setYCenter(0.0);
-        
+
         Graphics2D g2 = (Graphics2D)g;
         Rectangle2D.Double outer_border = new Rectangle2D.Double(0, 0, applet_width - 1, applet_height - 1);
         g2.draw(outer_border); 
+        g2.drawString("(" + String.valueOf(cursor_board_coords_x) + ", " + String.valueOf(cursor_board_coords_y) + ")", 50, 50);
+        // g2.drawString("(" + String.valueOf(cursor_normal_x) + ", " + String.valueOf(cursor_normal_y) + ")", 50, 80);
         BoardDrawer.drawBoard(g2, new BoundingBox(0.0, applet_width - 1.0, 0.0, applet_height - 1.0), camera, board);
-   }
+    }
 
-   private Random randomizer;
-   private TileBank bank;
-   private GameBoard board;
-   private TileIcon icon;
-   private int counter_x;
-   private int counter_y;
-   private CameraPosition camera;
+    //    public void update(Graphics g)
+    //    {
+    // 
+    //    }
+
+    private Random randomizer;
+    private TileBank bank;
+    private GameBoard board;
+    private TileIcon icon;
+    private int counter_x;
+    private int counter_y;
+    private CameraPosition camera;
+    private double cursor_normal_x;
+    private double cursor_normal_y;
+    private int cursor_board_coords_x;
+    private int cursor_board_coords_y;
+    private boolean valid_board_coords;
 } 
