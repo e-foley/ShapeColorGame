b7c48409078c72265d8feccce28f7ec620f112a2
e-foley <efoley@calpoly.edu>
2016-11-20 01:34:53 -0800

    introduce RackDrawer
    Built in much the same spirit as the BoardDrawer.  Not complete: needs
    to generate tile graphics still.

---
+++ RackDrawer.java
@@ -0,0 +1,16 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+// TODO: Make this class more sophisticated, including some sort of way of arranging tiles
+// TODO: Add setters and getters
+public class RackDrawer {
+    public RackDrawer() {}
+
+    public static void drawRack(Graphics2D g, Rack rack, double x_left, double y_top, double tile_size) {
+        for (int i = 0; i < rack.numPieces(); ++i) {
+            TileGraphic tile = new TileGraphic(new SquareIcon(Color.GRAY), 0.6);
+            tile.draw(g, x_left + (2*i + 1) * tile_size / 2.0, y_top + tile_size / 2.0, tile_size / 2.0, tile_size / 2.0);
+        }
+    }
+}
